96b9b76688b1870c8661e7b98ebbbbdc95ef7dc2
Carlo Bobba <carlo.bobba@outlook.it>
2023-02-12 14:30:32 +0100

    First commit

---
+++ src/test/java/it/proconsole/resourceclassifier/ResourceClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package it.proconsole.resourceclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceClassifierApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
