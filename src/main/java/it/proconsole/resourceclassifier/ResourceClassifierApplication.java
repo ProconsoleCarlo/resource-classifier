9b513d4a06a6b0987b62674f10a3fcdbe74fc375
Carlo Bobba <carlo.bobba@outlook.it>
2023-02-12 14:30:32 +0100

    First commit

---
+++ src/main/java/it/proconsole/resourceclassifier/ResourceClassifierApplication.java
@@ -0,0 +1,13 @@
+package it.proconsole.resourceclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceClassifierApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResourceClassifierApplication.class, args);
+  }
+
+}
